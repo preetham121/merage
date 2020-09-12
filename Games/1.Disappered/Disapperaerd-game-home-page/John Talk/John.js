ebc6e534fa478fe552778795a9c713d05e66ccd1
unknown <preetham.k.1211@gmail.com>
2020-09-12 23:09:56 +0530

    first commit

--- Games/1.Disappered/Disapperaerd-game-home-page/John Talk/John.js
+++ Games/1.Disappered/Disapperaerd-game-home-page/John Talk/John.js
@@ -6,43 +6,28 @@ back.addEventListener("click",()=>{
 const  messages = document.querySelector(".Messages");
 const listAppear= document.querySelector(".cloner");
 let  chooseUserText = document.getElementById("choose-message");
-let UserTalks;
-let userSide;
 let computerSide  = document.querySelector(".Computer-side");
 let body  = document.getElementById("body");
+
+
+
+
+
+//local storage values of userInput 
+let UserNameSaved = localStorage.getItem("userName");
+let userMaleSaved = localStorage.getItem("userMale");
+let userFemaleSaved = localStorage.getItem("userFemale");
+
+
+
+
+
+
 //Arrays
 // Texts
-let computerText = ["Hey!","My name is John. I’m from Rosewood.","good"];
-let UserText = ["Hey!","Do I know you?"];
-let chooseText = ["Hey!","Do I know you?"];
-
-//Id's
-/* let chooseID =  ["o0","o1","o2" ,"o3" ,"o4" ,"o5"];
-let JohnId = ["john0","john1","john3","john4","john5"] */
-
-
-
-
-//Creating functions 
-
-/* 
-let UserTextContent = () =>{
-    UserSide = document.createElement("div");
-    UserSide.setAttribute("class", " usersidetext");
-    UserSide.setAttribute("id", "userId0 ");
-    container.appendChild(UserSide);
-    UserTalks = document.createElement("div");
-    UserTalks.textContent = "dsfh";
-   UserTalks.setAttribute("class", "text-box-user text-select");
-   UserSide.appendChild(UserTalks);
-    let img = document.createElement("img");
-    img.setAttribute("src","./../../../../pics/images.png");
-    img.setAttribute("width","100%");
-    img.setAttribute("height","100%");
-    UserSide.appendChild(img);
-   
-}
- */
+let computerText = ["Hey!","My name is John. I’m from Rosewood.", `Are you detective ${UserNameSaved}`];
+let UserText = ["Hey!","Do I know you?","Hello John!", `Hello John, My name is ${UserNameSaved}`,"How did you get my number"];
+let chooseText = ["Hey!","Do I know you?","Hello John!" ,`Hello John, My name is ${UserNameSaved}`,"How did you get my number" ];
 
 
 
@@ -51,75 +36,259 @@ let UserTextContent = () =>{
 
 let pName = document.querySelector(".pName");
 const  c0 =  document.getElementById("c0"); 
+const  c1 =  document.getElementById("c1"); 
+const  c2 =  document.getElementById("c2"); 
+const  c3 =  document.getElementById("c3"); 
 const comp0 = document.getElementById("comp0");
 const comp1 = document.getElementById("comp1");
+const comp2 = document.getElementById("comp2");
 const john0 = document.getElementById("john0");
+const john1 = document.getElementById("john1");
+const john2 = document.getElementById("john2");
 const o0=document.getElementById("o0");
 const o1 = document.getElementById("o1");
+const o2 = document.getElementById("o2");
+const o3 = document.getElementById("o3");
+const o4 = document.getElementById("o4");
+o3.innerHTML = `Hello John, My name is ${UserNameSaved}`;
 const u0 = document.getElementById("u0");
+const u1 = document.getElementById("u1");
+const u2 = document.getElementById("u2");
+const u3 = document.getElementById("u3");
 const contain0 = document.getElementById("contain0");
 const contain1 = document.getElementById("contain1");
+const contain2 = document.getElementById("contain2");
+const contain3 = document.getElementById("contain3");
 const user0 = document.getElementById("user0");
 const user1 = document.getElementById("user1");
+const user2 = document.getElementById("user2");
+const user3 = document.getElementById("user3");
 const i0 = document.getElementById("i0");
+const i1 = document.getElementById("i1");
+let selection = document.getElementById("popup");
+
+const male = document.getElementById("Male");
+const female = document.getElementById("Female");
+const NAME = document.getElementById("NAME");
+const btnStrt = document.querySelector(".start");
+const control = document.querySelector(".control");
+var  maleValue ;
+let femaleValue ;
+
+
+btnStrt.addEventListener("click",()=>{
+
+if(NAME.value != ""){
+    localStorage.setItem("userName",NAME.value);
+
+ if(male.checked === true && female.checked ===true){
+     alert("Refresh the page and choose only one gender");
+ }
+   else if(male.checked === true){
+     localStorage.setItem("userMale",male.value);
+     maleValue = "UserIsMale";
+
+    }
+else if (female.checked === true){
+    localStorage.setItem("userFemale",female.value);
+}    
+else if(male.checked === false || female.checked === false){
+    alert("choose your gender");
+}
+else{
+    alert("re enter");
+}
+}
+
+else{
+    alert("Re enter your name and gender")
+}
+
+ control.style.display = "none";
+
+})
+
+
+
+
+
+
+
 
 
 if(c0.textContent.trim() === computerText[0].trim()){
     comp0.style.display = "flex";
-    
-   listAppear.addEventListener("click",()=>{
+selection.style.display = "block";
+   listAppear.addEventListener("click",()=>{    
        messages.style.display="block";    
        o0.style.display = "block";
        o1.style.display = "block";
-     
     })
 
 }
+
 
 if(o0.textContent.trim() === chooseText[0].trim()){
-   o0.addEventListener("click",()=>{
-       user0.style.display = "block";
-      localStorage.setItem("contain0",contain0.innerHTML);
-        messages.style.display = "none";
-       o0.remove();
+    o0.addEventListener("click",()=>{
+        user0.style.display = "block";
+       localStorage.setItem("contain0",contain0.innerHTML);
+         messages.style.display = "none";
+        o0.remove();
+  })}
 
+
+ 
+     
+    if(o1.textContent.trim() === chooseText[1].trim()){
+        o1.addEventListener("click",()=>{
+         user1.style.display = "block";
+         localStorage.setItem("contain1",contain1.innerHTML);
+         if (u0.textContent === UserText[0]){
+         o0.remove();
+         o1.remove();
+         messages.remove();
+       i0.style.display="flex";
+         setTimeout(()=>{
+           i0.remove();
+            comp1.style.display = "flex";
+            c1.textContent = computerText[1];
+            const profileImg = document.querySelector(".Profile-img");
+            profileImg.style.right = "32%";
+           
+            pName.textContent = "JOHN";
+            localStorage.setItem("nameOfChar",pName.innerHTML);
+            localStorage.setItem("john0",john0.innerHTML);
+            location.reload();
+         
+         },1000 ) 
+        }
+        })
+    
+        
+ 
+    }
+     
+
+
+
+    if (UserNameSaved){
+        if ( userMaleSaved){
+    control.style.display = "none"; 
+    if(o2.textContent.trim() === chooseText[2].trim()){
+        o2.addEventListener("click",()=>{
+            user2.style.display = "block";
+            localStorage.setItem("contain2",contain2.innerHTML);
+            messages.style.display ="none";
+            if(u2.textContent.trim() === UserText[2].trim()){
+                i1.style.display="flex";
+                setTimeout(()=>{
+                    i1.remove();
+
+                    comp2.style.display = "flex";
+                    c2.textContent = computerText[2];
+                    localStorage.setItem("john1",john1.innerHTML);
+                   
+                 
+                 },1000 ) 
+            }              
+      })
+    }
+
+    if (o3.textContent.trim() === chooseText[3].trim()){
+        o3.addEventListener("click",()=>{
+         user3.textContent = UserText[3];
+            user3.style.display = "block";
+            localStorage.setItem("contain3",contain3.innerHTML);
+            messages.style.display ="none"; 
+            if(u3.textContent.trim() === UserText[3].trim()){
+                  listAppear.addEventListener("click", ()=>{
+                    messages.style.display = "block";
+                    o4.style.display = "block";
+
+                  })
+               
+            }             
+
+
+           
+               })
+           
+               
+        
+           
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+    
+
+    
+
+
+    
+
+      
+         /* if (userFemaleSaved){
+            control.style.display = "none";
+            if(o2.textContent.trim() === chooseText[2].trim()){
+                o2.addEventListener("click",()=>{
+               alert("box");
+                })
+             
+             }
+        } */
    
 
-   })
-}
-
-
-
-
-
- if(o1.textContent.trim() === chooseText[1].trim()){
-    o1.addEventListener("click",()=>{
-     user1.style.display = "block";
-     localStorage.setItem("contain1",contain1.innerHTML);
-     if (u0.textContent === UserText[0]){
-     o0.remove();
-     o1.remove();
-     messages.remove();
-   i0.style.display="flex";
-     setTimeout(()=>{
-       i0.remove();
-        comp1.style.display = "block";
-    
-        const profileImg = document.querySelector(".Profile-img");
-        profileImg.style.right = "32%";
-       
-        pName.textContent = "JOHN";
-        localStorage.setItem("nameOfChar",pName.innerHTML);
-        localStorage.setItem("john0",john0.innerHTML);
-
-     
-     },1200 ) 
     }
-    })
+     
 
     
 
- } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -128,20 +297,58 @@ if(o0.textContent.trim() === chooseText[0].trim()){
 
 
  // Local storage
- pName.textContent = localStorage.getItem("nameOfChar");
  let savedUser0 = localStorage.getItem("contain0");
  let savedUser1 = localStorage.getItem("contain1");
+ let savedUser2 = localStorage.getItem("contain2");
+ let savedUser3 = localStorage.getItem("contain3");
  let savedComp0 = localStorage.getItem("john0");
+ let savedComp1 = localStorage.getItem("john1");
 
- if (savedUser0){
+
+pName.textContent = localStorage.getItem("nameOfChar");
+
+ 
+  if (savedUser0){
      contain0.innerHTML = savedUser0;
      o0.remove();
+   
+
  } 
   if (savedUser1){
      contain1.innerHTML = savedUser1;
      o1.remove();
      o0.remove();
  }
+
+if(savedUser2){
+    contain2.innerHTML = savedUser2;
+    o2.remove();
+    o3.remove();
+}
+
+if(savedUser3){
+    contain3.innerHTML = savedUser3;
+    o2.remove();
+    o3.remove();
+    o4.style.display = "block";
+}
+
+
+
+
+
+
   if (savedComp0){
       john0.innerHTML = savedComp0; 
+      o2.style.display = "block";
+     o3.style.display = "block"; 
+/*      o3.innerHTML = "Hello John, My name is " + UserNameSaved; */
   }
+  
+
+  if (savedComp1){
+    john1.innerHTML = savedComp1; 
+
+    
+}
+
--- Games/1.Disappered/Disapperaerd-game-home-page/game-home.js
+++ Games/1.Disappered/Disapperaerd-game-home-page/game-home.js
@@ -12,23 +12,55 @@ phone.addEventListener("click", () => {
 
 });
 
-let textNumber = document.querySelector(".text-number");
- function retrive(){ 
- let queer = localStorage.getItem("MessageRecent");
- let textMessage = document.querySelector(".text-message");
- textMessage.innerHTML = queer; 
- textNumber.style.display = "none";
-};
+let computerText = ["Hey!","My name is John. I’m from Rosewood."];
+let UserText = ["Hey!","Do I know you?","Hello John!"];
+
  
 
    
 let John = document.getElementById("john");
 John.addEventListener("click", ()=>{
-   /*  let ere = localStorage.removeItem("numbers");
-  textNumber.remove(); */
-  window.location.assign("./John Talk/John.html");
-
+  window.location.replace("./John Talk/John.html");
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Local Storage
+
 let name = document.getElementById("proName");
+let textNumber = document.querySelector(".text-number");
+let message = document.querySelector("#NumberDisplay");
+let savedUser0 = localStorage.getItem("contain0");
+let savedUser1 = localStorage.getItem("contain1");
+let savedComp0 = localStorage.getItem("john0");
+
+
 name.innerHTML = localStorage.getItem("nameOfChar");
+
+  if (savedUser0){
+        message.innerHTML = UserText[0];
+        textNumber.style.display = "none";
+
+  }
+
+  if(savedUser1){
+    message.innerHTML = UserText[1];
+        textNumber.style.display = "none";
+  }
+ 
+
+ if(savedComp0){
+  message.innerHTML = computerText[1];
+  textNumber.style.display = "block";
+ }
